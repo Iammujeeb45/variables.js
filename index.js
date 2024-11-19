3e23ce9563a72e898678a6790b5bd12768626ef4
Iammujeeb45 <mujeeb.hardwin@gmail.com>
2024-11-19 18:56:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+// // first class JavaScript variables
+
+// document.write(
+//   '<h1 style="color: red;  text-decoration: underline;">  Iam   </h1>'
+// );
+// document.write("<h2> Mujeeb </h2>");
+// document.write("<h3> New  </h3>");
+// document.write("<h4> To </h4>");
+// document.write("<h5> Java Script </h5>");
+
+// let a = 1;
+// let b = 2;
+// let c = 32;
+// let d = 43;
+// let e = 66;
+// let f = 78;
+
+// {
+//   const a = 100;
+//   let b = 200;
+//   b = 32;
+//   let d = 43;
+//   let e = 66;
+//   console.log(a + b);
+// }
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a % b);
+// console.log(b + c);
+// console.log(e + f);
+
+// const snno = 1;
+// const studentname = "mujeeb";
+// const schoolname = "citizen public high school";
+// const schoolAddress = "kotamitta mecleans road ";
+// console.table([snno, studentname, schoolAddress, schoolname]);
